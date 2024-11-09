623fcb5ac085ddd9caab219a029dd54841ef4522
paulsaji200 <paulsaji1791@gmail.com>
2024-11-09 08:07:47 +0530

    Add jw

--- utils/generateAdmintoken.js
+++ utils/generateAdmintoken.js
@@ -5,9 +5,10 @@ export const generateadminToken = (res, adminD) => {
     console.log(adminD)
     const token = jwt.sign({email: adminD.email, name: adminD.name, isadmin: adminD.isadmin }, "secretKey", {expiresIn: "3h"})
      
-    res.cookie('token', token, {
+    res.cookie("jwtToken", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        maxAge: 60*60*1000
+        secure: true,
+        maxAge: 60 * 60 * 1000,
+        sameSite: 'None'
     })
 }
