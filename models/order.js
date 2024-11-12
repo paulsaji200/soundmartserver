1d6f7b15f7b0754eec7f77eb8f4eec9369a1d62c
paulsaji200 <paulsaji1791@gmail.com>
2024-11-12 14:27:01 +0530

    orderId222

--- models/order.js
+++ models/order.js
@@ -2,7 +2,8 @@ const orderSchema = new mongoose.Schema({
   order_ID: {
     type: String,
     unique: true,
-    required: false,  // Temporarily set to false to avoid validation error
+    required: false,  
+    
   },
   user: {
     type: mongoose.Schema.Types.ObjectId,
